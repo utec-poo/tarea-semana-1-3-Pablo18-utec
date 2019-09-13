4f2b6b674e9967ab2264ef4c47290ebc62454df9
Pablo18-utec <55298885+Pablo18-utec@users.noreply.github.com>
2019-09-13 17:17:33 -0500

    Update main.cpp

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main() {
 
-entero base=0, entero altura=0;
+entero base=0, entero altura=0, entero area;
 cout << "\ningrese la base:";
 cin >> base;
 cout << "\ningrese la altura:";
 cin >> altura;
-
+area=(base*altura)/2;  
+cout<<"el area de un triangulo es"<<area;
 
 // ToDO
 
